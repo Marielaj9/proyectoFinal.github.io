c8d016c097920a1da4588c3c9b493e9190413686
Mariela Jara <115422695+Marielaj9@users.noreply.github.com>
2024-02-29 13:58:37 -0300

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,10 @@
+function turnLogout(element) {
+    element.innerText = "Logout";
+}
+function turnOff(element) {
+    element.innerText = "Off";
+}
+
+function hide(element) {
+    element.remove();
+}
